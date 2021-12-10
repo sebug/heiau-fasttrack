6c3b82f75dc85361b2dddfc184ae58f81856f4c5
sebug <sebastian.gfeller@gmail.com>
2021-12-10 15:17:05 +0100

    Variable rules

---
+++ 1Z0-149/03. declaring variables/01. some variables.sql
@@ -0,0 +1,10 @@
+-- Starting with 12c, most identifiers can be 128 bytes now (before
+-- it was 30 letters)
+-- can contain special characters $, _, #
+DECLARE
+my_fancy_and_very_long$variable#withspecialchars number;
+BEGIN
+my_fancy_and_very_long$variable#withspecialchars := 42;
+my_fancy_and_very_long$variable#withspecialchars := my_fancy_and_very_long$variable#withspecialchars + 5;
+dbms_output.put_line(my_fancy_and_very_long$variable#withspecialchars);
+END;

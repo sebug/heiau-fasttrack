f2fee2381dc59617c12f2bbbc559305bbcadece8
sebug <sebastian.gfeller@gmail.com>
2021-12-10 14:48:50 +0100

    SQL Plus specificities

--- 1Z0-149/02._pl_sql_overview/01. anonymous_blocks.sql
+++ 1Z0-149/02._pl_sql_overview/01. anonymous_blocks.sql
@@ -2,3 +2,16 @@ begin
 dbms_output.put_line('lookee, an anonymous block');
 dbms_output.put_line('can have more than one, of course');
 end;
+
+declare
+begin
+dbms_output.put_line('Can also have an empty declare');
+end;
+
+declare
+v number;
+begin
+v := 47;
+dbms_output.put('The answer to live, the universe and everything, adjusted for inflation is');
+dbms_output.put_line(v);
+end;
---
+++ 1Z0-149/02._pl_sql_overview/02. sqlplus_specificities.sql
@@ -0,0 +1,4 @@
+-- when running in sqlplus, use a
+-- / to terminate and actually run the stuff
+
+set serveroutput on -- to actually show the output

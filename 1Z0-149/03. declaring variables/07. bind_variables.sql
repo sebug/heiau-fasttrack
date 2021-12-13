5f51150251ac3e5e1e23d263a6610940e95a22ec
sebug <sebastian.gfeller@gmail.com>
2021-12-13 13:22:40 +0100

    Set autoprint on

--- 1Z0-149/03. declaring variables/07. bind_variables.sql
+++ 1Z0-149/03. declaring variables/07. bind_variables.sql
@@ -1,4 +1,5 @@
-VARIABLE b_result number;
+VARIABLE b_result number; -- for variables, can't have precision in numbers
+-- you can use set autoprint on to automatically print variable content
 begin
 select (salary * 12) + nvl(commission_pct, 0) into :b_result
 FROM employees where employee_id = 144;

ca10c5fb5a3706454ed8e16e4e06cb4a9cce7ede
sebug <sebastian.gfeller@gmail.com>
2021-12-14 16:13:18 +0100

    Null comparison

--- 1Z0-149/06. control structures/03. null_comparison.sql
+++ 1Z0-149/06. control structures/03. null_comparison.sql
@@ -1,6 +1,7 @@
 DECLARE
 x NUMBER := 5;
 y NUMBER;
+b BOOLEAN;
 BEGIN
     IF x <> y THEN
         dbms_output.put_line('X not equal Y');
@@ -8,5 +9,11 @@ BEGIN
         dbms_output.put_line('X equal Y');
     ELSE
         dbms_output.put_line('nope, nope');
+    END IF;
+    
+    b := x <> y;
+    
+    IF b IS NULL THEN
+        dbms_output.put_line('comparison value is null, which is falsy');
     END IF;
 END;

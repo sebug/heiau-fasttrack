004e4e60015fb55a5ea046583b975d976e2c45bc
sebug <sebastian.gfeller@gmail.com>
2021-12-14 17:28:42 +0100

    With for loop

--- 1Z0-149/06. control structures/05. loop_de_loop.sql
+++ 1Z0-149/06. control structures/05. loop_de_loop.sql
@@ -14,3 +14,38 @@ BEGIN
     END LOOP;
     dbms_output.put_line('fib(' || n || ') = ' || x);
 END;
+
+/
+
+DECLARE
+n NUMBER := &number_of_iterations;
+x NUMBER := 0;
+y NUMBER := 1;
+counter NUMBER := 0;
+tmp NUMBER;
+BEGIN
+    WHILE counter <= n LOOP
+        tmp := x;
+        x := y;
+        y := x + tmp;
+        counter := counter + 1;
+    END LOOP;
+    dbms_output.put_line('fib(' || n || ') = ' || x);
+END;
+
+/
+
+DECLARE
+n NUMBER := &number_of_iterations;
+x NUMBER := 0;
+y NUMBER := 1;
+tmp NUMBER;
+BEGIN
+    FOR i in 0 .. n
+    LOOP
+        tmp := x;
+        x := y;
+        y := x + tmp;
+    END LOOP;
+    dbms_output.put_line('fib(' || n || ') = ' || x);
+END;

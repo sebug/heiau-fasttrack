3dad36083dd5f8320eac4c787d694af5d1f5377c
sebug <sebastian.gfeller@gmail.com>
2021-12-14 09:13:48 +0100

    Named blocks

---
+++ 1Z0-149/04. executable statements/02. named_blocks.sql
@@ -0,0 +1,12 @@
+begin <<outer>>
+declare
+the_number number := 23;
+begin
+    declare
+    the_number number := 42;
+    begin
+        dbms_output.put_line(outer.the_number);
+        dbms_output.put_line(the_number);
+    end;
+end;
+end;

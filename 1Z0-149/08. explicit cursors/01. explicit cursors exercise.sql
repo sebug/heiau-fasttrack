db9b0214715bfd76e66093654d2455411a0ab4b1
sebug <sebastian.gfeller@gmail.com>
2021-12-17 14:45:10 +0100

    Fetching into row type

--- 1Z0-149/08. explicit cursors/01. explicit cursors exercise.sql
+++ 1Z0-149/08. explicit cursors/01. explicit cursors exercise.sql
@@ -4,18 +4,17 @@ WHERE department_id = 30;
 
 DECLARE
 CURSOR c_emp_dpt30 IS
-SELECT employee_id, first_name FROM employees
+SELECT * FROM employees
 WHERE department_id = 30;
 
-v_empno employees.employee_id%TYPE;
-v_first_name employees.first_name%TYPE;
+v_emp_rec employees%ROWTYPE;
 BEGIN
     OPEN c_emp_dpt30;
     
     LOOP
-        FETCH c_emp_dpt30 INTO v_empno, v_first_name;
+        FETCH c_emp_dpt30 INTO v_emp_rec;
         EXIT WHEN c_emp_dpt30%NOTFOUND; -- note the exit before the print -- the last time around the loop, we don't fetch anything into the variables.
-        dbms_output.put_line(v_empno || '-' || v_first_name);
+        dbms_output.put_line(c_emp_dpt30%ROWCOUNT || ' ' || v_emp_rec.first_name || '-' || v_emp_rec.last_name);
     END LOOP;
     
     CLOSE c_emp_dpt30;

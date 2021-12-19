7eef3ecc4a826ae4ff77d64af2a91561943548df
sebug <sebastian.gfeller@gmail.com>
2021-12-19 14:20:45 +0100

    raise_application_error as well

--- 1Z0-149/09. exceptions/04. user defined exceptions.sql
+++ 1Z0-149/09. exceptions/04. user defined exceptions.sql
@@ -15,3 +15,17 @@ BEGIN
         WHEN e_invalid_no THEN
             dbms_output.put_line('As expected, invalid number');
 END;
+/
+
+DECLARE
+    v_employee_id NUMBER := 1;
+BEGIN
+    UPDATE employees
+    SET salary = 2000
+    WHERE employee_id = v_employee_id;
+    
+    dbms_output.put_line(SQL%ROWCOUNT);
+    IF SQL%NOTFOUND THEN
+        raise_application_error(-20000, 'Invalid employee ID');
+    END IF;
+END;

30dd0347e123f3c001795e5f8d4cef38edc79d70
sebug <sebastian.gfeller@gmail.com>
2021-12-19 13:41:39 +0100

    Triggering the exceptions

--- 1Z0-149/09. exceptions/01. predefined exceptions.sql
+++ 1Z0-149/09. exceptions/01. predefined exceptions.sql
@@ -4,8 +4,14 @@ BEGIN
     SELECT first_name
     INTO v_employee_firstname
     FROM employees
-    WHERE employee_id = -1;
+    WHERE first_name = &name;
+    
+    dbms_output.put_line(v_employee_firstname);
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
-        dbms_output.put_line('Oh, nothing to see here');
+            dbms_output.put_line('Oh, nothing to see here');
+        WHEN TOO_MANY_ROWS THEN
+            dbms_output.put_line('Too much to see here');
+        WHEN OTHERS THEN
+            dbms_output.put_line('Something else');
 END;

ee66ba4698a73d4cc2e36c568bc6af689cace6b5
sebug <sebastian.gfeller@gmail.com>
2021-12-19 18:19:27 +0100

    Further examples

---
+++ 1Z0-149/11. functions/02. further examples.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION get_salary_tax
+(p_salary employees.salary%TYPE)
+RETURN NUMBER
+IS
+BEGIN
+    IF p_salary < 5000 THEN
+        RETURN p_salary * (10 / 100);
+    ELSE
+        RETURN p_salary * (15 / 100);
+    END IF;
+END;
+
+SELECT first_name, last_name, salary, get_salary_tax(salary)
+FROM employees;

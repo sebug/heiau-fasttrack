9784c3b15dad85200389968d2e12754e16b6b14d
sebug <sebastian.gfeller@gmail.com>
2021-12-19 14:03:21 +0100

    pragma exception init

---
+++ 1Z0-149/09. exceptions/03. non predefined errors.sql
@@ -0,0 +1,10 @@
+DECLARE
+    e_insert EXCEPTION;
+    PRAGMA exception_init(e_insert, -01400);
+BEGIN
+    INSERT INTO DEPARTMENTS (DEPARTMENT_ID, DEPARTMENT_NAME)
+    VALUES (277, NULL);
+    EXCEPTION
+        WHEN e_insert THEN
+            dbms_output.put_line('It complained about inserting NULL');
+END;

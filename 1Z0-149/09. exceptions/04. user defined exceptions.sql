559db5205413eab062432356359916b119972de4
sebug <sebastian.gfeller@gmail.com>
2021-12-19 14:16:07 +0100

    User defined exceptions

---
+++ 1Z0-149/09. exceptions/04. user defined exceptions.sql
@@ -0,0 +1,17 @@
+DECLARE
+    v_employee_id NUMBER := 1;
+    e_invalid_no EXCEPTION;
+BEGIN
+    UPDATE employees
+    SET salary = 2000
+    WHERE employee_id = v_employee_id;
+    
+    dbms_output.put_line(SQL%ROWCOUNT);
+    IF SQL%NOTFOUND THEN
+        RAISE e_invalid_no;
+    END IF;
+    
+    EXCEPTION
+        WHEN e_invalid_no THEN
+            dbms_output.put_line('As expected, invalid number');
+END;

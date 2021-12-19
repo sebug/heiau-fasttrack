467c7d00828c7ad3b88ea13d665350589e3c7dc4
sebug <sebastian.gfeller@gmail.com>
2021-12-19 15:25:51 +0100

    IN is default

--- 1Z0-149/10. procedures/01. update_sal.sql
+++ 1Z0-149/10. procedures/01. update_sal.sql
@@ -1,8 +1,13 @@
 CREATE OR REPLACE PROCEDURE update_sal
-(p_emp_id IN NUMBER, p_amount IN NUMBER)
+(p_emp_id NUMBER, p_amount NUMBER)
 IS
 BEGIN
     UPDATE employees
     SET salary = salary + p_amount
     WHERE employee_id = p_emp_id;
+    COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line(SQLCODE);
+            dbms_output.put_line(SQLERRM);
 END;
---
+++ 1Z0-149/NOTES.md
@@ -0,0 +1,8 @@
+# Notes Course
+
+## Stored procedures
+In SQL*Plus, you can use show errors to show the compilation errors (they
+are also in the user_errors table).
+
+You can use ed to edit the last command.
+

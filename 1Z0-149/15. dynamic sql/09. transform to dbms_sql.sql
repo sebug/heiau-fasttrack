5b29661c07ac5c51f75ca99e0b881f6772d6a9d0
sebug <sebastian.gfeller@gmail.com>
2021-12-20 17:11:42 +0100

    Cursor ID

--- 1Z0-149/15. dynamic sql/09. transform to dbms_sql.sql
+++ 1Z0-149/15. dynamic sql/09. transform to dbms_sql.sql
@@ -7,7 +7,9 @@ BEGIN
     v_cur_id := dbms_sql.open_cursor;
     dbms_sql.parse(v_cur_id, 'DELETE FROM ' || p_table_name, dbms_sql.native);
     v_no_rec := dbms_sql.execute(v_cur_id);
-    dbms_output.put_line(v_no_rec || ' records deleted from table ' || p_table_name);
+    dbms_output.put_line(v_no_rec || ' records deleted from table ' || p_table_name || ' using cursor ' ||
+        v_cur_id);
+    COMMIT;
 END;
 
 EXECUTE delete_any_table2('emp1');

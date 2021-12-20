355d667d49e04a12fe1e19591259f77ecf3d620a
sebug <sebastian.gfeller@gmail.com>
2021-12-20 12:54:00 +0100

    Handling no data found with exception - butt-ugly

--- 1Z0-149/14. oracle supplied packages/03. files.sql
+++ 1Z0-149/14. oracle supplied packages/03. files.sql
@@ -13,3 +13,26 @@ BEGIN
     dbms_output.put_line('Line 2 is ' || v_text);
     utl_file.fclose(v_file);
 END;
+
+/
+
+DECLARE
+    v_file UTL_FILE.FILE_TYPE;
+    v_location VARCHAR2(100) := 'MYDIR';
+    v_filename VARCHAR2(100) := 'sample.txt';
+    v_text VARCHAR2(32767);
+    i INTEGER := 1;
+BEGIN
+    v_file := utl_file.fopen(v_location, v_filename, 'r');
+    BEGIN
+        LOOP
+            utl_file.get_line(v_file, v_text);
+            dbms_output.put_line('Line ' || i || ' is ' || v_text);
+            i := i + 1;
+        END LOOP;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                dbms_output.put_line('End of file reached');
+    END;
+    utl_file.fclose(v_file);
+END;

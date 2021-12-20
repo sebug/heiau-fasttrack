082dad332f1cb1faa83ffc52d812636c55c82418
sebug <sebastian.gfeller@gmail.com>
2021-12-20 17:08:50 +0100

    DBMS SQL

---
+++ 1Z0-149/15. dynamic sql/09. transform to dbms_sql.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PROCEDURE delete_any_table2
+(p_table_name VARCHAR2)
+IS
+v_no_rec NUMBER;
+v_cur_id NUMBER;
+BEGIN
+    v_cur_id := dbms_sql.open_cursor;
+    dbms_sql.parse(v_cur_id, 'DELETE FROM ' || p_table_name, dbms_sql.native);
+    v_no_rec := dbms_sql.execute(v_cur_id);
+    dbms_output.put_line(v_no_rec || ' records deleted from table ' || p_table_name);
+END;
+
+EXECUTE delete_any_table2('emp1');

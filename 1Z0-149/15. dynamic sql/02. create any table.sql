83c9765a9158cc4bf93f1160ecd44fde72a6ac10
sebug <sebastian.gfeller@gmail.com>
2021-12-20 15:00:15 +0100

    Multiline strings

--- 1Z0-149/15. dynamic sql/02. create any table.sql
+++ 1Z0-149/15. dynamic sql/02. create any table.sql
@@ -4,10 +4,12 @@ IS
     v_details VARCHAR2(30000);
 BEGIN
     v_details := 'CREATE TABLE ' || p_table_name ||
-       ' (' || p_details || ')';
+       ' (
+       ' || p_details || '
+       )';
     dbms_output.put_line(v_details);
     EXECUTE IMMEDIATE v_details;
 END;
 
-EXECUTE create_any_table('emp3', 'emp_id NUMBER');
+EXECUTE create_any_table('emp4', 'emp_id NUMBER');
 

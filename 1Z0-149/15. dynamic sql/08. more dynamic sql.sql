f63d167ebfda187d5873d2b8cf1a1ac447dc047b
sebug <sebastian.gfeller@gmail.com>
2021-12-20 16:45:48 +0100

    Immediateception

--- 1Z0-149/15. dynamic sql/08. more dynamic sql.sql
+++ 1Z0-149/15. dynamic sql/08. more dynamic sql.sql
@@ -3,7 +3,8 @@ BEGIN
     BEGIN
         EXECUTE IMMEDIATE ''
         BEGIN
-        dbms_output.put_line(''''Hey hey'''');
+        dbms_output.put_line(''''I heard you like execute immediate,
+        so I put an execute immediate in your execute immediate'''');
         END;
         '';
     END;';

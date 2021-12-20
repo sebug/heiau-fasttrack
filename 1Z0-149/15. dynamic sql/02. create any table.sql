5b8c88eceb4384822c719bdfa64bcef27e7ea9e2
sebug <sebastian.gfeller@gmail.com>
2021-12-20 14:58:07 +0100

    Create any table

---
+++ 1Z0-149/15. dynamic sql/02. create any table.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PROCEDURE CREATE_ANY_TABLE
+(p_table_name VARCHAR2, p_details VARCHAR2)
+IS
+    v_details VARCHAR2(30000);
+BEGIN
+    v_details := 'CREATE TABLE ' || p_table_name ||
+       ' (' || p_details || ')';
+    dbms_output.put_line(v_details);
+    EXECUTE IMMEDIATE v_details;
+END;
+
+EXECUTE create_any_table('emp3', 'emp_id NUMBER');
+

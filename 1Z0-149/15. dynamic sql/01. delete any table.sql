d1f6af1d5ab0445b63069ecb524de9ac37e35e0b
sebug <sebastian.gfeller@gmail.com>
2021-12-20 14:49:11 +0100

    Explicitly dealing with the exception

--- 1Z0-149/15. dynamic sql/01. delete any table.sql
+++ 1Z0-149/15. dynamic sql/01. delete any table.sql
@@ -28,13 +28,20 @@ CREATE OR REPLACE PROCEDURE delete_any_table
 (p_table_name VARCHAR2)
 IS
     v_no_rec NUMBER;
+    e_table_does_not_exist EXCEPTION;
+    PRAGMA exception_init(e_table_does_not_exist, -00942);
 BEGIN
     EXECUTE IMMEDIATE 'DELETE FROM ' || p_table_name;
     v_no_rec := SQL%ROWCOUNT;
     COMMIT;
     dbms_output.put_line(v_no_rec || ' records deleted from table ' || p_table_name);
+    EXCEPTION
+        WHEN e_table_does_not_exist THEN
+            dbms_output.put_line('Table ' || p_table_name || ' does not exist');
 END;
 
 EXECUTE delete_any_table('emp2');
 
 SELECT * FROM emp2;
+
+EXECUTE delete_any_table('nobody_is_home');

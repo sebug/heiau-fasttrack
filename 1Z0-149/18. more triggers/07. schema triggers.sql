a56f2546ce7ca250068a90106aaec27986739bcd
sebug <sebastian.gfeller@gmail.com>
2021-12-21 18:46:28 +0100

    DDL Log

--- 1Z0-149/18. more triggers/07. schema triggers.sql
+++ 1Z0-149/18. more triggers/07. schema triggers.sql
@@ -9,4 +9,29 @@ END;
 
 ALTER TRIGGER before_create_trigger DISABLE;
 
-CREATE TABLE test5 ( id NUMBER );
+DROP TABLE ddl_log;
+
+CREATE TABLE ddl_log (
+    operation VARCHAR2(30),
+    obj_owner VARCHAR2(30),
+    object_name VARCHAR2(30),
+    attempt_by VARCHAR2(30),
+    attempt_date DATE
+);
+
+CREATE OR REPLACE TRIGGER ddl_trigger
+AFTER DDL
+ON SCHEMA
+BEGIN
+    INSERT INTO ddl_log
+    SELECT ora_sysevent,
+    ora_dict_obj_owner,
+    ora_dict_obj_name,
+    user,
+    sysdate
+    FROM dual;
+END;
+
+CREATE TABLE t5 ( id NUMBER );
+
+SELECT * FROM ddl_log;

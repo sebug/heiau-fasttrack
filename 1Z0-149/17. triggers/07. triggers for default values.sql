c4ae048b02afae6c9a00bc1d06be027b136f16ef
sebug <sebastian.gfeller@gmail.com>
2021-12-21 16:16:04 +0100

    Meh

---
+++ 1Z0-149/17. triggers/07. triggers for default values.sql
@@ -0,0 +1,20 @@
+DROP TABLE customers;
+
+CREATE TABLE customers (
+    cust_id NUMBER,
+    name VARCHAR2(100),
+    status CHAR(1)
+);
+
+CREATE OR REPLACE TRIGGER customers_default_status
+BEFORE
+INSERT
+ON customers
+FOR EACH ROW
+BEGIN
+    :new.status := 'Y';
+END;
+
+INSERT INTO customers(cust_id, name) VALUES (1, 'seb');
+
+SELECT * FROM customers;

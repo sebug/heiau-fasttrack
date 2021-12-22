43b0e572ea8f9ff9b727cdc12d3f71c14748602b
sebug <sebastian.gfeller@gmail.com>
2021-12-22 11:57:01 +0100

    Natively compiled

---
+++ 1Z0-149/19. the compiler/02. compile type.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM user_procedures;
+
+SELECT *
+FROM user_plsql_object_settings
+WHERE PLSQL_CODE_TYPE <> 'INTERPRETED';
+
+CREATE OR REPLACE PROCEDURE p1
+IS
+BEGIN
+    dbms_output.put_line('Yay from p1');
+END;
+
+SELECT *
+FROM user_plsql_object_settings
+WHERE name = 'P1';
+
+ALTER SESSION SET plsql_code_type = native;
+
+ALTER PROCEDURE p1 COMPILE;
+
+SELECT *
+FROM user_plsql_object_settings
+WHERE PLSQL_CODE_TYPE <> 'INTERPRETED';
+
+EXECUTE p1;
